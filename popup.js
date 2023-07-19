5152c250619ad2ede664d7e3af1aaf5c6658db03
Lucas1114 <lucas.feb4@gmail.com>
2023-07-19 21:52:44 +1000

    replace alert with extension display

--- popup.js
+++ popup.js
@@ -1,23 +1,92 @@
-let scrapeEmails = document.getElementById('scrapeEmails');
-let list = document.getElementById('emailList');
-
-// var bkg = chrome.extension.getBackgroundPage();
-// var console = chrome.extension.getBackgroundPage().console;
+let getGameScore = document.getElementById('getGameScore');
+let list = document.getElementById('score');
 
 
 
-// Add an event listener to the button
+chrome.runtime.onMessage.addListener((request,sender,sendResponse)=>{
 
-  
+  let gameScore = request.gameScore;
+  let url = request.url;
 
 
-  
+  // Create a button element
+  const button = document.createElement('button');
+  button.textContent = gameScore;
+
+  // Add a click event listener to the button
+  button.addEventListener("click", function() {
+    // Open a website in a new tab or window
+    window.open(url+'/user-reviews');
+  });
+
+
+  // Append the link to a container element
+  list.appendChild(button);
+
+
+
+
+
+
+  // let li1 = document.createElement('li');
+  // li1.innerText = gameScore;
+  // list.appendChild(li1);
+
+  // let li2 = document.createElement('li');
+  // li2.innerText = url+'/user-reviews'
+  // list.appendChild(li2);
+      
+});
+
+getGameScore.addEventListener("click",async()=>{
+
+  getGameInfo().then(result => {
+    var gameInfo = result;
+    
+    if(gameInfo===null)
+    {
+      alert("Game Score is only effective on a certain game page of ebgames.com.au.")
+    }
+    else
+    {
+      const gameName = gameInfo[0].replace(/ /g, "-").replace(/:/g, "").replace(/\u00F6/g, "o").replace('-(preowned)', "").toLowerCase();
+      var platform = gameInfo[1].toString();
+
+      if(platform==="Nintendo Switch"){
+        platform = "switch"
+      }
+      else{
+        platform = gameInfo[1].replace(/ /g, "-").toLowerCase();
+      }
+      
+      const gameUrlOfMetacritic = 'https://www.metacritic.com/game/'+platform+'/'+gameName;
+      
+      chrome.runtime.sendMessage(gameUrlOfMetacritic);
+
+      // const gameUrlOfMetacritic = 'https://www.metacritic.com/game/playstation-5/ea-sports-fc-24'
+      // const gameUrlOfMetacritic = 'https://www.metacritic.com/game/playstation-5/god-of-war-ragnarok'
+      const userScoreClassNameOfMetacritic ='metascore_w user large game'
+
+      //assemble url
+      //deal new game without info
+      
+      getScore(gameUrlOfMetacritic,userScoreClassNameOfMetacritic);
+
+    }
+  });
+
+    // const scriptContent =getWebpageScript(url);
+    //use the game name to make up of the new url, retrive the info of this url
+    //get the score from the info(RegEx???)
+    //place the score on the 
+    //multiple game website, attache link url
+})
 
 
 
 function getGameInfo() {
   var gameInfo=111;
-
+  
   return new Promise((resolve,reject)=>{
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
@@ -62,7 +131,7 @@ function extractContent() {
 }
 
 
-function getGameScore(url, className) {
+async function getScore(url, className) {
   return fetch(url)
       .then(response => response.text())
       .then(html => {
@@ -70,125 +139,46 @@ function getGameScore(url, className) {
         const doc = parser.parseFromString(html, 'text/html');
 
         const gameScoreElement = doc.getElementsByClassName(className);
+        let gameScore;
         
-
         if(gameScoreElement.length === 0){
-          alert("No score until game release")
-          // alert(doc.getElementsByClassName('countdown_msg')[0].textContent);
-
+          gameScore = "No score until game release"
+          
         }else{
-          const gameScore = gameScoreElement[0].textContent;
-          chrome.runtime.sendMessage(gameScore);
-          alert(gameScore);
-
-        }
+          gameScore = gameScoreElement[0].textContent;
+          
+        }  
+        return gameScore;
         
       })
+      .then(async(gameScore) =>{
+        let [tab] = await chrome.tabs.query({
+          active:true, currentWindow: true
+        });
+    
+        chrome.scripting.executeScript({
+            target:{tabId:tab.id},
+            func:sendResult,
+            args:[gameScore,url]            
+        });
+
+      })
+      
       .catch(error => {
-        console.error('Error fetching webpage:', error);
+        console.error('An error occurred:', error);
       });
 }
 
 
 
-
-
-
-
-scrapeEmails.addEventListener("click",async()=>{
-
-
-  getGameInfo().then(result => {
-    var gameInfo = result;
-    
-    if(gameInfo===null)
-    {
-      alert("Game Score is only effective on a certain game page of ebgames.com.au.")
-    }
-    else
-    {
-      
-      const gameName = gameInfo[0].replace(/ /g, "-").replace(/:/g, "").replace(/\u00F6/g, "o").replace('-(preowned)', "").toLowerCase();
-      var platform = gameInfo[1].toString();
-
-
-      // chrome.runtime.sendMessage(platform.length);
-      
-
-      // for (let i = 0; i < platform.length; i++) {
-      //   let character = platform[i];
-      //   chrome.runtime.sendMessage(character);
-      //   chrome.runtime.sendMessage(i);
-      // }
-      
-     
-      if(platform==="Nintendo Switch"){
-        platform = "switch"
-        
-      }
-      else{
-        platform = gameInfo[1].replace(/ /g, "-").toLowerCase();
-        
-      }
-      
-
-      const gameUrlOfMetacritic = 'https://www.metacritic.com/game/'+platform+'/'+gameName;
-      
-      chrome.runtime.sendMessage(gameUrlOfMetacritic);
-
-      // const gameUrlOfMetacritic = 'https://www.metacritic.com/game/playstation-5/ea-sports-fc-24'
-      // const gameUrlOfMetacritic = 'https://www.metacritic.com/game/playstation-5/god-of-war-ragnarok'
-      const userScoreClassNameOfMetacritic ='metascore_w user large game'
-
-      //assemble url
-      //deal new game without info
-
-      getGameScore(gameUrlOfMetacritic,userScoreClassNameOfMetacritic);
-
-    }
-  });
-
-  
-    
-    
-    
-
-    
-
-    
-    
-    // const scriptContent =getWebpageScript(url);
-
-    
-
-    //use the game name to make up of the new url, retrive the info of this url
-
-    
-
-    //get the score from the info(RegEx???)
-
-
-
-
-    //place the score on the 
-
-    //multiple game website, attache link url
-})
-
-
-
-
-/*
-function scrapeEmailsFromPage(){
-    
-    const emailRegEx = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/gim;
-
-    
-    let emails = document.body.innerHTML.match(emailRegEx);
-   
-    chrome.runtime.sendMessage({emails});
-    
-
+function sendResult(gameScore,url){
+  chrome.runtime.sendMessage({gameScore,url});
+  // chrome.runtime.sendMessage(12345);
 }
 
-*/
+
+
+
+
+
+
